f43d1f297b3b52eb7349e274e1a6c4b3477f1050
SaikaAhmed <saikaaridii.com>
2020-08-07 00:54:17 +0600

    undefined-vs-null

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+//undefined
+ let name;
+ console.log(name);
+
+ function add(num1, num2){
+     console.log(num1 + num2);
+     return;
+ }
+ const result = add(10,20);
+ console.log(result);
+
+ function add(num1, num2){
+     console.log(num1,num2);
+ }
+ const result = add(13);
+ console.log(result);
+
+const person = { name: "saika", phone: 03948};
+console.log(person.address);
+
+const family = ['ammu', 'abbu', 'brother'];
+console.log(family[11]);
